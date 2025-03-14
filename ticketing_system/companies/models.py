1424108736a845d03d270999308f29891ca51174
Antoine Delrue <antoine@a-delrue.be>
2025-03-14 14:35:40 +0100

    Minor fix

--- ticketing_system/companies/models.py
+++ ticketing_system/companies/models.py
@@ -1,5 +1,7 @@
 import uuid
 from django.db import models
+from django.core.validators import RegexValidator
+
 
 class Company(models.Model):
     id = models.UUIDField(
@@ -7,8 +9,12 @@ class Company(models.Model):
         default=uuid.uuid4,
         editable=False
     )
-    name = models.CharField(max_length=255)
-    initials = models.CharField(max_length=3, unique=True)
+    name = models.CharField(max_length=255)    
+    initials = models.CharField(
+        max_length=3,
+        unique=True,
+        validators=[RegexValidator(regex=r"^[A-Z]{2,3}$", message="Must be 2-3 uppercase letters")]
+    )
     address = models.TextField(blank=True, null=True)
     contact_phone = models.CharField(max_length=20, blank=True, null=True)
 
